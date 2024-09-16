5a22e5549418c7b8a4aee503ffcbaace9f2937d6
sahelzaff <sahelzaffar2@gmail.com>
2024-09-17 00:27:15 +0530

    added a status page to server

--- Hearing_test/app.py
+++ Hearing_test/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS  # Import CORS
+from flask import Flask, request, jsonify, send_file, render_template
+from flask_cors import CORS
 import os
 from utils import generate_pdf_report
 
@@ -34,5 +34,13 @@ def submit_hearing_test():
     # Return the report
     return send_file(pdf_file, as_attachment=True)
 
+# Route for status page
+@app.route('/')
+def status():
+    return render_template('status.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Set host to '0.0.0.0' to make the app available externally
+    # Port should be set by the environment variable PORT, default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
